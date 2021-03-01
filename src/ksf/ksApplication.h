4aebc094e78f8a190ec93c95806ddc15dd17f764
cziter15 <chris.streh@gmail.com>
2021-03-01 18:34:04 +0100

    Revert looping

--- src/ksf/ksApplication.h
+++ src/ksf/ksApplication.h
@@ -8,16 +8,12 @@
 	{														\
 		while(application.loop())							\
 		{													\
-			if (delayTime == 0)								\
-				yield();									\
-			else											\
-				delay(delayTime);							\
+			delay(delayTime);								\
 		}													\
 	}														\
 }
 
 #define RUN_APP_BLOCKING_LOOPED(appClass) RUN_APP_BLOCKING_LOOPED_TIME(appClass, 1)
-#define RUN_APP_BLOCKING_LOOPED_NODELAY(appClass)  RUN_APP_BLOCKING_LOOPED_TIME(appClass, 0)
 
 namespace ksf
 {
