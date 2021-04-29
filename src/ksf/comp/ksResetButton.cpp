3882b51637a9cc7424537a1b075e0963260fc3f2
cziter15 <chris.streh@gmail.com>
2021-04-29 07:18:25 +0200

    Reset button tweaks

--- src/ksf/comp/ksResetButton.cpp
+++ src/ksf/comp/ksResetButton.cpp
@@ -2,24 +2,65 @@
 #include "ksResetButton.h"
 #include "Arduino.h"
 
+#ifdef ESP32
+#include "SPIFFS.h"
+#include "WiFi.h"
+#else
+#include "FS.h"
+#include "Esp8266WiFi.h"
+#endif
+
+#include "Esp.h"
+
 namespace ksf
 {
+	#define SHORT_TRIGGER 50
+	#define LONG_TRIGGER 5000
+
 	ksResetButton::ksResetButton(unsigned char pin, unsigned char triggerBy) 
 		: buttonPin(pin), triggerState(triggerBy)
 	{
+
 	}
 
 	bool ksResetButton::init(ksComposable* owner)
 	{
 		pinMode(buttonPin, INPUT);
+		lastState = digitalRead(buttonPin);
 		return true;
 	}
 
 	bool ksResetButton::loop()
 	{
-		// If push button is pressed, then then break app (goes to config app)
-		if (digitalRead(buttonPin) == triggerState)
-			return false;
+		unsigned char currentState = digitalRead(buttonPin);
+
+		if (currentState != lastState)
+		{
+			if (currentState == triggerState)
+			{
+				pressedTime = millis();
+			}
+			else if (currentState != triggerState)
+			{
+				releasedTime = millis();
+
+				unsigned long pressDuration = releasedTime - pressedTime;
+
+				if (pressDuration > LONG_TRIGGER)
+				{
+					WiFi.disconnect(true);
+					SPIFFS.format();
+					ESP.restart();
+					return false;
+				}
+				else if (pressDuration > SHORT_TRIGGER)
+				{
+					return false;
+				}
+			}
+
+			lastState = currentState;
+		}
 
 		return true;
 	}
--- src/ksf/comp/ksResetButton.h
+++ src/ksf/comp/ksResetButton.h
@@ -8,6 +8,9 @@ namespace ksf
 		protected:
 			unsigned char buttonPin = 0;
 			unsigned char triggerState = 0;
+			unsigned char lastState = 0;
+			unsigned long pressedTime = 0;
+			unsigned long releasedTime = 0;
 
 		public:
 			ksResetButton(unsigned char pin, unsigned char triggerBy);
