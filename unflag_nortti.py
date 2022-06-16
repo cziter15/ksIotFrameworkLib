90e67b591173406967bd7567cb6fc09cb1180c0d
cziter15 <chris.streh@gmail.com>
2022-06-16 19:57:43 +0200

    Add exception to unflag_nortti.py

--- unflag_nortti.py
+++ unflag_nortti.py
@@ -1,2 +1,11 @@
-Import("projenv")
-projenv.ProcessUnFlags("-fno-rtti")
+try:
+	Import("projenv", "env")
+
+	for e in (env, DefaultEnvironment(), projenv):
+		e.ProcessUnFlags("-fno-rtti")
+
+except BaseException as err:
+	print("\033[91m[ksIotFrameworkLib:] Error while trying unflag_nortti.py!\033[0m")
+	print("\033[33m[ksIotFrameworkLib:] Are you on latest platformio dev?\033[0m")
+	print(f"\033[33m[ksIotFrameworkLib:] {err}\033[0m")
+	pass
