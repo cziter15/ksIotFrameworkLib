20f3595fbb3621d340ad2ab2938592534c4c5e99
cziter15 <chris.streh@gmail.com>
2022-06-19 13:38:05 +0200

    Add rtti_fix.py, now properly handled by latest platformio dev.

---
+++ rtti_fix.py
@@ -0,0 +1,13 @@
+# This script unflags -fno-rtti, because this library requires RTTI support.
+try:
+	Import("projenv", "env")
+
+	print("\033[92m[ksIotFrameworkLib:] Processing nortti unflag!\033[0m")
+
+	for e in (env, DefaultEnvironment(), projenv):
+		e.ProcessUnFlags("-fno-rtti")
+
+	print("\033[92m[ksIotFrameworkLib:] Done, unflagged for all environments!\033[0m")
+except BaseException as err:
+	print("\033[91m[ksIotFrameworkLib:] Error while trying rtti_fix.py!\033[0m")
+	print("\033[33m[ksIotFrameworkLib:] Are you on latest platformio dev?\033[0m")
