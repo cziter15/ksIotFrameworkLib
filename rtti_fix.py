2e0261e62855b1ad73fb768cafbd44420b1ca15c
Krzysiek S <chris.streh@gmail.com>
2022-06-21 14:48:08 +0200

    Better message for Platformio core

--- rtti_fix.py
+++ rtti_fix.py
@@ -10,4 +10,4 @@ try:
 	print("\033[92m[ksIotFrameworkLib:] Done, unflagged for all environments!\033[0m")
 except BaseException as err:
 	print("\033[91m[ksIotFrameworkLib:] Error while trying rtti_fix.py!\033[0m")
-	print("\033[33m[ksIotFrameworkLib:] Are you on latest platformio dev?\033[0m")
+	print("\033[33m[ksIotFrameworkLib:] Platformio in version v6.0.3b dev (at least) is required.\033[0m")
