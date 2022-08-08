4447165287a11ff99d135ec5fecb761624627bb4
cziter15 <chris.streh@gmail.com>
2022-08-08 14:53:11 +0200

    Unify copyright header

--- src/ksf/comp/ksWifiConnector.cpp
+++ src/ksf/comp/ksWifiConnector.cpp
@@ -1,10 +1,10 @@
 /*
  *	Copyright (c) 2021-2022, Krzysztof Strehlau
- *	
- *	This file is part of the ksIotFramework library.
- *	All licensing information can be found inside LICENSE.md file
  *
- * 	https://github.com/cziter15/ksIotFrameworkLib/blob/master/LICENSE
+ *	This file is part of the ksIotFramework library.
+ *	All licensing information can be found inside LICENSE.md file.
+ *
+ *	https://github.com/cziter15/ksIotFrameworkLib/blob/master/LICENSE
  */
 
 #include "../ksComposable.h"
