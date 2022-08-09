fe3f72878f5a6228a2cd1e87b342f6ecd5ed37e2
cziter15 <chris.streh@gmail.com>
2022-08-09 14:50:52 +0200

    Cleanup extraScript structure

--- pio-scripts/extraScript.py
+++ pio-scripts/extraScript.py
@@ -5,15 +5,17 @@
 #
 #	https://github.com/cziter15/ksIotFrameworkLib/blob/master/LICENSE
 
+from logger import *
+
 try:
 	Import("projenv", "env")
 
-	print("\033[92m[ksIotFrameworkLib:] Processing nortti unflag!\033[0m")
+	ksPrintLog(Colors.Green, "Processing nortti unflag!")
 
 	for e in (env, DefaultEnvironment(), projenv):
 		e.ProcessUnFlags("-fno-rtti")
 
-	print("\033[92m[ksIotFrameworkLib:] Done, unflagged for all environments!\033[0m")
+	ksPrintLog(Colors.Green, "Done, unflagged for all environments.")
 except BaseException as err:
-	print("\033[91m[ksIotFrameworkLib:] Error while trying rtti_fix.py!\033[0m")
-	print("\033[33m[ksIotFrameworkLib:] Platformio in version v6.0.3b dev (at least) is required.\033[0m")
+	ksPrintLog(Colors.Red, "Error while executing fix_rtti script. Are you on newest platformio core?")
+	ksPrintLog(Colors.Yellow, str(err))
