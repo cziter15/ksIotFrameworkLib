9a6952131a97642aa864b4a4082dee9c43d4fd34
cziter15 <chris.streh@gmail.com>
2023-01-24 17:02:33 +0100

    Get rid of module in ksAppRotator

--- src/ksf/ksAppRotator.h
+++ src/ksf/ksAppRotator.h
@@ -39,8 +39,12 @@ namespace ksf
 				if (currentApplication && currentApplication->loop())
 					return;
 
-				/* Switch to the next application. */
-				currentApplication = appSwpawners[appIndex++ % appSwpawners.size()]();
+				/* Set next index. */
+				if (++appIndex >= appSwpawners.size())
+					appIndex = 0;
+
+				/* Switch to next application. */
+				currentApplication = appSwpawners[appIndex]();
 
 				/* If not initialized, then destroy and try to do our business next delay. */
 				if (!currentApplication->init()) 
