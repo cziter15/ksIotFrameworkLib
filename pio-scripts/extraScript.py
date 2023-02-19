0402d284b2193f95301c79cfb92af910105baa0f
cziter15 <chris.streh@gmail.com>
2023-02-19 17:46:04 +0100

    Fix es

--- pio-scripts/extraScript.py
+++ pio-scripts/extraScript.py
@@ -22,8 +22,8 @@ try:
 	flagCounter = 0
 	for e in (env, DefaultEnvironment(), projenv):
 		e.ProcessFlags("-std=gnu++17")
-		e.ProcessFlags("-DNO_GLOBAL_ARDUINOOTA")
-		e.ProcessFlags("-DWM_NODEBUG")
+		e.ProcessFlags("-D NO_GLOBAL_ARDUINOOTA")
+		e.ProcessFlags("-D WM_NODEBUG")
 		flagCounter += 1
 	ksPrintLog(Colors.Magenta, "Successfully added flags for [" + str(flagCounter) + "] environments.")
 
