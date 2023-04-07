2b6a719130d16835c50c24880a9605d8de2c2e5a
cziter15 <chris.streh@gmail.com>
2023-04-07 14:11:41 +0200

    ksDevStat - in progress

---
+++ src/ksf/comp/ksDevStatMqttReporter.cpp
@@ -0,0 +1,44 @@
+/*
+ *	Copyright (c) 2021-2023, Krzysztof Strehlau
+ *
+ *	This file is part of the ksIotFramework library.
+ *	All licensing information can be found inside LICENSE.md file.
+ *
+ *	https://github.com/cziter15/ksIotFrameworkLib/blob/master/LICENSE
+ */
+
+#include "Arduino.h"
+#include "../ksApplication.h"
+#include "ksDevStatMqttReporter.h"
+#include "ksMqttConnector.h"
+
+namespace ksf::comps
+{
+	ksDevStatMqttReporter::ksDevStatMqttReporter(uint8_t intervalInSeconds) 
+		: reporterTimer(intervalInSeconds * KSF_ONE_SECOND_MS)
+	{}
+
+	bool ksDevStatMqttReporter::init(ksApplication* owner)
+	{
+		this->owner = owner;
+		return true;
+	}
+
+	void ksDevStatMqttReporter::postInit()
+	{
+		mqttConnWp = owner->findComponent<ksMqttConnector>();
+
+		if (auto mqttConnSp{mqttConnWp.lock()})
+			mqttConnSp->onConnected->registerEvent(connEventHandle, std::bind(&ksDevStatMqttReporter::onConnected, this));
+	}
+	
+	void ksDevStatMqttReporter::onConnected()
+	{
+		reporterTimer.restart();
+	}
+	
+	bool ksDevStatMqttReporter::loop()
+	{
+		return true;
+	}
+}
---
+++ src/ksf/comp/ksDevStatMqttReporter.h
@@ -0,0 +1,59 @@
+/*
+ *	Copyright (c) 2021-2023, Krzysztof Strehlau
+ *
+ *	This file is part of the ksIotFramework library.
+ *	All licensing information can be found inside LICENSE.md file.
+ *
+ *	https://github.com/cziter15/ksIotFrameworkLib/blob/master/LICENSE
+ */
+
+#pragma once
+
+#include "../evt/ksEvent.h"
+#include "../ksComponent.h"
+#include "../ksSimpleTimer.h"
+
+namespace ksf::comps
+{
+	class ksMqttConnector;
+	class ksDevStatMqttReporter : public ksComponent
+	{
+		KSF_RTTI_DECLARATIONS(ksDevStatMqttReporter, ksComponent)
+
+		protected:
+			ksApplication* owner{nullptr};							// Pointer to application that owns this component.
+			std::weak_ptr<ksMqttConnector> mqttConnWp;				// Weak pointer to MQTT connector.
+			std::shared_ptr<evt::ksEventHandle> connEventHandle;	// Event handle for connection delegate.
+			
+			ksSimpleTimer reporterTimer;
+
+			/*
+				Function called on MQTT connection.
+			*/
+			void onConnected();
+
+		public:
+			ksDevStatMqttReporter(uint8_t intervalInSeconds = 15);
+
+			/*
+				Initializes MQTT debug responder component.
+
+				@param owner Pointer to ksApplication object that owns this component.
+				@return True on success, false on fail.
+			*/
+			bool init(ksApplication* owner) override;
+
+			/*
+				Method called after component initialization.
+				Used to setup message callbacks.
+			*/
+			void postInit() override;
+
+			/*
+				Handles MQTT debug connector component loop logic.
+
+				@return True on success, false on fail.
+			*/
+			bool loop() override;
+	};
+}
