fdbd7053fb7b8f8368a2f1eeaa28eb570416ca2c
cziter15 <chris.streh@gmail.com>
2023-09-25 11:12:15 +0200

    Cleanup

--- src/ksf/comp/ksDevicePortal.cpp
+++ src/ksf/comp/ksDevicePortal.cpp
@@ -32,6 +32,7 @@
 #include "ESPAsyncWebServer.h"
 #include "../res/otaWebpage.h"
 
+#define DP_PSTR(x) (String(FPSTR(x)).c_str())
 namespace ksf::comps
 {
 	ksDevicePortal::ksDevicePortal()
@@ -86,55 +87,54 @@ namespace ksf::comps
 	{
 		server = std::make_shared<AsyncWebServer>(80);
 
-		//static const char* username{"admin"};
 		server->onNotFound([&](AsyncWebServerRequest *request) {
-			AsyncWebServerResponse *response = request->beginResponse(302, "text/plain", "");
-			response->addHeader("Location", "/");
+			AsyncWebServerResponse *response = request->beginResponse(302, FPSTR("text/plain"), "");
+			response->addHeader(FPSTR("Location"), "/");
 			request->send(response);
 		});
 
 		server->on("/", HTTP_GET, [&](AsyncWebServerRequest *request) {
 			AsyncWebServerResponse *response 
 			{ 
-				request->beginResponse_P(200, "text/html", DEVICE_FRONTEND_HTML, DEVICE_FRONTEND_HTML_SIZE)
+				request->beginResponse_P(200, FPSTR("text/html"), DEVICE_FRONTEND_HTML, DEVICE_FRONTEND_HTML_SIZE)
 			};
 
-			response->addHeader("Content-Encoding", "gzip");
+			response->addHeader(FPSTR("Content-Encoding"), FPSTR("gzip"));
 			request->send(response);
 		});
 
-		server->on("/api/identity", HTTP_GET, [&](AsyncWebServerRequest *request) {
+		server->on(DP_PSTR("/api/identity"), HTTP_GET, [&](AsyncWebServerRequest *request) {
 			String json;
-			json += "{ \"id\": \"";
+			json += FPSTR("{ \"id\": \"");
 			json += WiFi.getHostname();
-			json += "\", \"hardware\": \"" HARDWARE "\" }";
+			json += FPSTR("\", \"hardware\": \"" HARDWARE "\" }");
 			request->send(200, "application/json", json);
 		});
 
-		server->on("/api/getMode", HTTP_GET, [&](AsyncWebServerRequest *request) {
+		server->on(DP_PSTR("/api/getMode"), HTTP_GET, [&](AsyncWebServerRequest *request) {
 			bool isAPMode = WiFi.getMode() == WIFI_AP;
 			String json;
-			json += "{ \"isAPMode\": ";
-			json += isAPMode ? "true" : "false";
-			json += " }";
-			request->send(200, "application/json", json);
+			json += FPSTR("{ \"isAPMode\": ");
+			json += isAPMode ? FPSTR("true") : FPSTR("false");
+			json += FPSTR(" }");
+			request->send(200, FPSTR("application/json"), json);
 		});
 
-		server->on("/api/goToConfigMode", HTTP_GET, [&](AsyncWebServerRequest *request) {
+		server->on(DP_PSTR("/api/goToConfigMode"), HTTP_GET, [&](AsyncWebServerRequest *request) {
 			breakApp = true;
-			request->send(200, "application/json", "{}");
+			request->send(200, FPSTR("application/json"), "{}");
 		});
 
-		server->on("/api/getDeviceParams", HTTP_GET, [this](AsyncWebServerRequest *request) {
+		server->on(DP_PSTR("/api/getDeviceParams"), HTTP_GET, [&](AsyncWebServerRequest *request) {
 			std::vector<std::weak_ptr<ksConfigProvider>> configCompsWp;
 			owner->findComponents<ksConfigProvider>(configCompsWp);
 
 			String json;
-			json += "{\"ssid\":\"";
+			json += FPSTR("{\"ssid\":\"");
 			json += WiFi.SSID();
-			json += "\", \"password\":\"";
+			json += FPSTR("\", \"password\":\"");
 			json += WiFi.psk();
-			json += "\",\"params\": [";
+			json += FPSTR("\",\"params\": [");
 
 			for (auto& configCompWp : configCompsWp)
 			{
@@ -147,10 +147,9 @@ namespace ksf::comps
 
 				for (auto& parameter : configCompSp->getParameters())
 				{
-					json += "{";
-					json += "\"id\": \"";
+					json += FPSTR("{\"id\": \"");
 					json += String(parameter.id.c_str());
-					json += "\", \"value\": \"";\
+					json += FPSTR("\", \"value\": \"");
 
 					if (parameter.value.empty())
 						json += String(parameter.defaultValue.c_str());
@@ -166,16 +165,16 @@ namespace ksf::comps
 
 			json += "]}";
 
-			request->send(200, "application/json", json);
+			request->send(200, FPSTR("application/json"), json);
 		});
 
-		server->on("/api/saveConfig", HTTP_POST, [this](AsyncWebServerRequest *request) {
-			String ssid = request->arg("ssid");
-			String password = request->arg("password");
+		server->on(DP_PSTR("/api/saveConfig"), HTTP_POST, [&](AsyncWebServerRequest *request) {
+			String ssid = request->arg(FPSTR("ssid"));
+			String password = request->arg(FPSTR("password"));
 
 			if (ssid.isEmpty())
 			{
-				request->send(200, "application/json", "{ \"result\" : \"Empty SSID\" }");
+				request->send(200, FPSTR("application/json"), FPSTR("{ \"result\" : \"Empty SSID\" }"));
 				return;
 			}
 				
@@ -185,6 +184,8 @@ namespace ksf::comps
 			std::vector<std::weak_ptr<ksConfigProvider>> configCompsWp;
 			owner->findComponents<ksConfigProvider>(configCompsWp);
 
+			String paramPrefix{FPSTR("param_")};
+
 			for (auto& configCompWp : configCompsWp)
 			{
 				auto configCompSp{configCompWp.lock()};
@@ -193,7 +194,7 @@ namespace ksf::comps
 
 				for (auto& parameter : configCompSp->getParameters())
 				{
-					String value{request->arg(String("param_") + parameter.id.c_str())};
+					String value{request->arg(paramPrefix + parameter.id.c_str())};
 					if (value.isEmpty())
 						continue;
 
@@ -203,59 +204,59 @@ namespace ksf::comps
 				configCompSp->saveParams();
 			}
 
-			request->send(200, "application/json", "{ \"result\": \"OK\" }");\
+			request->send(200, FPSTR("application/json"), FPSTR("{ \"result\": \"OK\" }"));
 
 			delay(500);
 			ESP.restart();
 		});
 
-		server->on("/api/scanNetworks", HTTP_GET, [&](AsyncWebServerRequest *request) {
+		server->on(DP_PSTR("/api/scanNetworks"), HTTP_GET, [&](AsyncWebServerRequest *request) {
 			switch (WiFi.scanComplete())
 			{
 				case WIFI_SCAN_FAILED:
 					WiFi.scanNetworks(true);
 				case WIFI_SCAN_RUNNING:
-					request->send(200, "text/plain", "");
+					request->send(500, FPSTR("text/plain"), "");
 				return;
 
 				default:
 				{
-					String json{"["};
+					String json;
+					json += '[';
 					for (int i{0}; i < WiFi.scanComplete(); ++i)
 					{
 						if (i > 0)
-							json += ",";
-						json += "{";
-						json += "\"rssi\":"+String(WiFi.RSSI(i));
-						json += ",\"ssid\":\""+WiFi.SSID(i)+"\"";
-						json += ",\"bssid\":\""+WiFi.BSSIDstr(i)+"\"";
-						json += ",\"channel\":"+String(WiFi.channel(i));
-						json += ",\"secure\":"+String(WiFi.encryptionType(i));
-						json += "}";
+							json += ',';
+						json += FPSTR("{\"rssi\":")+String(WiFi.RSSI(i));
+						json += FPSTR(",\"ssid\":\"")+WiFi.SSID(i)+"\"";
+						json += FPSTR(",\"bssid\":\"")+WiFi.BSSIDstr(i)+"\"";
+						json += FPSTR(",\"channel\":")+String(WiFi.channel(i));
+						json += FPSTR(",\"secure\":")+String(WiFi.encryptionType(i));
+						json += '}';
 					}
-					json += "]";
+					json +=	']';
 					WiFi.scanDelete();
-					request->send(200, "application/json", json);
+					request->send(200, FPSTR("application/json"), json);
 				}
 			}
 		});
 
-		server->on("/api/flash", HTTP_POST, [&](AsyncWebServerRequest *request) {
+		server->on(DP_PSTR("/api/flash"), HTTP_POST, [&](AsyncWebServerRequest *request) {
 			AsyncWebServerResponse *response = 
-				request->beginResponse((Update.hasError())?500:200, "text/plain", (Update.hasError())?"FAIL":"OK");
+				request->beginResponse((Update.hasError())?500:200, FPSTR("text/plain"), (Update.hasError())?"FAIL":"OK");
 
-			response->addHeader("Connection", "close");
-			response->addHeader("Access-Control-Allow-Origin", "*");
+			response->addHeader(FPSTR("Connection"), FPSTR("close"));
+			response->addHeader(FPSTR("Access-Control-Allow-Origin"), "*");
 			request->send(response);
 			ESP.restart();
 		}, [&](AsyncWebServerRequest *request, String filename, size_t index, uint8_t *data, size_t len, bool final) {
 			if (!index) 
 			{
 				if(!request->hasParam("MD5", true)) 
-					return request->send(400, "text/plain", "MD5 parameter missing");
+					return request->send(400, FPSTR("text/plain"), FPSTR("MD5 parameter missing"));
 
 				if(!Update.setMD5(request->getParam("MD5", true)->value().c_str())) 
-					return request->send(400, "text/plain", "MD5 parameter invalid");
+					return request->send(400, FPSTR("text/plain"),  FPSTR("MD5 parameter invalid"));
 
 				#if defined(ESP8266)
 					int cmd = U_FLASH;
@@ -267,21 +268,19 @@ namespace ksf::comps
 					int cmd = U_FLASH;
 					if (!Update.begin(UPDATE_SIZE_UNKNOWN, cmd)) { // Start with max available size
 				#endif
-					return request->send(400, "text/plain", "OTA could not begin");
+					return request->send(400,  FPSTR("text/plain"), FPSTR("OTA failed to start"));
 				}
 			}
 
 			// Write chunked data to the free sketch space
 			if(len)
 				if (Update.write(data, len) != len)
-					return request->send(400, "text/plain", "OTA could not begin");
+						return request->send(400,  FPSTR("text/plain"), FPSTR("OTA failed to start"));
 				
 			if (final) 
-			{ 
+			{
 				if (!Update.end(true)) 
-				{
-					return request->send(400, "text/plain", "Could not end OTA");
-				}
+					return request->send(400,  FPSTR("text/plain"), FPSTR("OTA failed to start"));
 			}
 			else return;
 		});
