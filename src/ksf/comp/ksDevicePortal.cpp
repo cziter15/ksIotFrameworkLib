f814ef392cd7d6ae0c1020c7252010cd621efab8
cziter15 <chris.streh@gmail.com>
2023-12-12 17:30:49 +0100

    Typo fix

--- src/ksf/comp/ksDevicePortal.cpp
+++ src/ksf/comp/ksDevicePortal.cpp
@@ -301,7 +301,7 @@ namespace ksf::comps
 			{
 #if APP_LOG_ENABLED
 				app->setLogCallback(std::bind(&ksDevicePortal::onAppLog, this, _1));
-				std::string log{PSTR("Logs enabled- both command responses and detailed logs will be printed.")};
+				std::string log{PSTR("Logs enabled - both command responses and detailed logs will be printed.")};
 #else
 				std::string log{PSTR("Detailed logs are disabled (no APP_LOG_ENABLED set). Only command responses will be printed.")};
 #endif
