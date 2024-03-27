a3babb6534dd602e92cd91c1c2510c28d6ee809e
cziter15 <cziter15@users.noreply.github.com>
2024-03-27 20:36:49 +0000

    deploy: e0d0c48b0f95995c1ed0d1fa73561ac5daf4bb69

--- search/all_3.js
+++ search/all_3.js
@@ -4,7 +4,6 @@ var searchData=
   ['devicename_1',['deviceName',['../classksf_1_1comps_1_1ks_wi_fi_configurator.html#a80263f9796319d18e19a851ff8ebf185',1,'ksf::comps::ksWiFiConfigurator']]],
   ['dnsserver_2',['dnsServer',['../classksf_1_1comps_1_1ks_device_portal.html#a9d9ecd302b4ef31f3778baf91eed0e48',1,'ksf::comps::ksDevicePortal']]],
   ['does_20it_20work_20under_20the_20hood_3',['How does it work under the hood?',['../index.html#autotoc_md7',1,'']]],
-  ['donate_4',['Donate',['../index.html#autotoc_md14',1,'']]],
-  ['driveasactivelow_5',['driveAsActiveLow',['../classksf_1_1comps_1_1ks_led.html#aab8014871bda50730d67de08fb995a39',1,'ksf::comps::ksLed']]],
-  ['driveaspushpull_6',['driveAsPushPull',['../classksf_1_1comps_1_1ks_led.html#a79747cb65b374896c24931539221efc6',1,'ksf::comps::ksLed']]]
+  ['driveasactivelow_4',['driveAsActiveLow',['../classksf_1_1comps_1_1ks_led.html#aab8014871bda50730d67de08fb995a39',1,'ksf::comps::ksLed']]],
+  ['driveaspushpull_5',['driveAsPushPull',['../classksf_1_1comps_1_1ks_led.html#a79747cb65b374896c24931539221efc6',1,'ksf::comps::ksLed']]]
 ];
