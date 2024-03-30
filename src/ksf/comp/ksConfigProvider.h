35c7ee9865501f690198b989d4c4993a9c660669
Krzysiek S <chris.streh@gmail.com>
2024-03-30 19:04:18 +0100

    Update ksConfigProvider.h

--- src/ksf/comp/ksConfigProvider.h
+++ src/ksf/comp/ksConfigProvider.h
@@ -33,6 +33,7 @@ namespace ksf::comps
 		};
 	};
 
+	/*! A structure that defines the configuration parameter. */
 	struct ksConfigParameter
 	{
 		std::string id;
@@ -42,6 +43,19 @@ namespace ksf::comps
 		int maxLength{};
 	};
 
+	/*!
+		ksConfigProvider is a component that provides a set of parameters to the ksWifiConfigurator 
+		and is also responsible for handling its storage.
+		
+		In the configurator application, it should be instantiated like a typical component, and ksWifiConfigurator must be added to the component stack. 
+		In this case, it will manage the correct flow of data reading and saving.
+		
+		In contrast, in a normal application, it can be created for a short period of time, usually inside the application's 
+		init method, to read the data and return the required value.
+		Refer to the EnergySensor init method for an example. Note that the component will be destroyed upon reaching the function return. 
+		Also, note that the setupRotations method can return false in case of a problem with the value retrieval (for example, no config present yet), thus 
+		causing the application to break and triggering the app rotator to move to the next defined application, usually AP-based configurator."
+	*/
 	class ksConfigProvider : public ksComponent
 	{
 		KSF_RTTI_DECLARATIONS(ksConfigProvider, ksComponent)
@@ -50,38 +64,39 @@ namespace ksf::comps
 			std::list<ksConfigParameter> params;
 			
 			/*!
-				@brief Defines new parameter.
+				@brief Defines new configutation parameter.
 
-				@param id Shared ID/Label to identify parameter
-				@param value Default value
-				@param maxLength Maximum length of parameter value
-				@param type Type of parameter
+				@param id Unique identificator that will be also used as property label by Device Portal.
+				@param value Default parameter value.
+				@param maxLength Maximum length of the value.
+				@param type Type of parameter, which tells the Device Portal which field type should be used.
 			*/
 			void addNewParam(std::string id, std::string value, int maxLength = 50, EConfigParamType::Type type = {});
 
 			/*!
-				@brief Defines new parameter (with default value).
-
-				@param config Reference to config
-				@param id Shared ID/Label to identify parameter
-				@param maxLength Maximum length of parameter value
-				@param type Type of parameter
+				@brief Defines new configuation parameter (with default value).
+				
+				@param config ksConfig reference.
+				@param id Unique identificator that will be also used as property label by Device Portal.
+				@param value Default parameter value.
+				@param maxLength Maximum length of the value.
+				@param type Type of parameter, which tells the Device Portal which field type should be used.		
 			*/
 			void addNewParamWithConfigDefault(ksConfig& config, std::string id, int maxLength = 50, EConfigParamType::Type type = {});
 
 		public:
 			/*!
-				@brief Retrieves const list of parameters.
+				@brief Provides const reference to the list of managed parameters.
 			*/
 			std::list<ksConfigParameter>& getParameters();
 
 			/*!
-				@brief Loads parameters from the config file.
+				@brief Populates the parameter list by processing the configuration file.
 			*/
 			virtual void readParams() = 0;
 
 			/*!
-				@brief Moves parameters to the config file.
+				@brief Outputs the parameter list to the configuration file.
 			*/
 			virtual void saveParams() = 0;
 	};
