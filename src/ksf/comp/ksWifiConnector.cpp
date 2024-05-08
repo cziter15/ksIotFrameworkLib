075debb4643c1021ece00bd54d9d9be01e53d142
cziter15 <chris.streh@gmail.com>
2024-05-08 23:42:51 +0200

    Fix dns errors

--- src/ksf/comp/ksWifiConnector.cpp
+++ src/ksf/comp/ksWifiConnector.cpp
@@ -23,6 +23,17 @@
 
 namespace ksf::comps
 {
+	inline void overrideDnsWithGoogleDns()
+	{
+		/* Define override DNS addresses. */
+		static const ip_addr primaryDns{LWIP_MAKEU32(8, 8, 8, 8)};
+		static const ip_addr secondaryDns{LWIP_MAKEU32(8, 8, 4, 4)};
+		
+		/* Set DNS servers. */
+		dns_setserver(0, &primaryDns);
+		dns_setserver(1, &secondaryDns);
+	}
+
 	ksWifiConnector::ksWifiConnector(const char* hostname, bool savePower) 
 		: savePower(savePower)
 	{
@@ -83,6 +94,7 @@ namespace ksf::comps
 
 	void ksWifiConnector::wifiConnectedInternal()
 	{
+		overrideDnsWithGoogleDns();
 		onConnected->broadcast();
 	}
 
