333f20933f6a2fd8a1e85b48fca7c797a7018837
cziter15 <cziter15@users.noreply.github.com>
2024-05-21 08:08:24 +0000

    deploy: 582f45fce43b6802d3bfc4b345e14840dfe2c0fe

--- search/all_d.js
+++ search/all_d.js
@@ -1,11 +1,11 @@
 var searchData=
 [
-  ['onanymessage_0',['onAnyMessage',['../classksf_1_1comps_1_1ks_mqtt_connector.html#ad8b5761c616a2107091f97d54600003c',1,'ksf::comps::ksMqttConnector']]],
+  ['onanymessage_0',['onAnyMessage',['../classksf_1_1comps_1_1ks_mqtt_connector.html#a700ac5e8f17b9ab152a8c5ecb1d9e1fa',1,'ksf::comps::ksMqttConnector']]],
   ['onapplog_1',['onAppLog',['../classksf_1_1comps_1_1ks_device_portal.html#a71a92f3fba9cf819aad4115ae41b9b9a',1,'ksf::comps::ksDevicePortal']]],
   ['onconnected_2',['onConnected',['../classksf_1_1comps_1_1ks_mqtt_connector.html#aeb2e99942201a89db129abe571115db6',1,'ksf::comps::ksMqttConnector::onConnected'],['../classksf_1_1comps_1_1ks_dev_stat_mqtt_reporter.html#a50a8b87a541070e20616a0815ce881d8',1,'ksf::comps::ksDevStatMqttReporter::onConnected()']]],
-  ['ondevicemessage_3',['onDeviceMessage',['../classksf_1_1comps_1_1ks_mqtt_connector.html#a068e0ad483a487bf6e643355799b2fc1',1,'ksf::comps::ksMqttConnector']]],
+  ['ondevicemessage_3',['onDeviceMessage',['../classksf_1_1comps_1_1ks_mqtt_connector.html#aff442ccec76a46a0834a21cd882e8ccd',1,'ksf::comps::ksMqttConnector']]],
   ['ondisconnected_4',['onDisconnected',['../classksf_1_1comps_1_1ks_mqtt_connector.html#aacb8a4b214c5ea6d71c74469fec167ce',1,'ksf::comps::ksMqttConnector']]],
-  ['onhandleportalcommand_5',['onHandlePortalCommand',['../classksf_1_1comps_1_1ks_device_portal.html#a5ead9113c131284ee325740154194c7d',1,'ksf::comps::ksDevicePortal']]],
+  ['onhandleportalcommand_5',['onHandlePortalCommand',['../classksf_1_1comps_1_1ks_device_portal.html#a48dce11cf0fbd3747ab5b5a82db32b54',1,'ksf::comps::ksDevicePortal']]],
   ['onrequest_5findex_6',['onRequest_index',['../classksf_1_1comps_1_1ks_device_portal.html#a80b467535ec6279ad36ffaa1c181ad90',1,'ksf::comps::ksDevicePortal']]],
   ['onrequest_5fnotfound_7',['onRequest_notFound',['../classksf_1_1comps_1_1ks_device_portal.html#a8feb8377e4cebd8c15e64071a62ae9fa',1,'ksf::comps::ksDevicePortal']]],
   ['onrequest_5fotachunk_8',['onRequest_otaChunk',['../classksf_1_1comps_1_1ks_device_portal.html#a874a08f061b795a96dc4badf7415fd82',1,'ksf::comps::ksDevicePortal']]],
--- search/variables_9.js
+++ search/variables_9.js
@@ -1,10 +1,10 @@
 var searchData=
 [
-  ['onanymessage_0',['onAnyMessage',['../classksf_1_1comps_1_1ks_mqtt_connector.html#ad8b5761c616a2107091f97d54600003c',1,'ksf::comps::ksMqttConnector']]],
+  ['onanymessage_0',['onAnyMessage',['../classksf_1_1comps_1_1ks_mqtt_connector.html#a700ac5e8f17b9ab152a8c5ecb1d9e1fa',1,'ksf::comps::ksMqttConnector']]],
   ['onconnected_1',['onConnected',['../classksf_1_1comps_1_1ks_mqtt_connector.html#aeb2e99942201a89db129abe571115db6',1,'ksf::comps::ksMqttConnector']]],
-  ['ondevicemessage_2',['onDeviceMessage',['../classksf_1_1comps_1_1ks_mqtt_connector.html#a068e0ad483a487bf6e643355799b2fc1',1,'ksf::comps::ksMqttConnector']]],
+  ['ondevicemessage_2',['onDeviceMessage',['../classksf_1_1comps_1_1ks_mqtt_connector.html#aff442ccec76a46a0834a21cd882e8ccd',1,'ksf::comps::ksMqttConnector']]],
   ['ondisconnected_3',['onDisconnected',['../classksf_1_1comps_1_1ks_mqtt_connector.html#aacb8a4b214c5ea6d71c74469fec167ce',1,'ksf::comps::ksMqttConnector']]],
-  ['onhandleportalcommand_4',['onHandlePortalCommand',['../classksf_1_1comps_1_1ks_device_portal.html#a5ead9113c131284ee325740154194c7d',1,'ksf::comps::ksDevicePortal']]],
+  ['onhandleportalcommand_4',['onHandlePortalCommand',['../classksf_1_1comps_1_1ks_device_portal.html#a48dce11cf0fbd3747ab5b5a82db32b54',1,'ksf::comps::ksDevicePortal']]],
   ['onupdateend_5',['onUpdateEnd',['../classksf_1_1comps_1_1ks_device_portal.html#a7dc2e53045a02c604627f8101baa68e2',1,'ksf::comps::ksDevicePortal']]],
   ['onupdatestart_6',['onUpdateStart',['../classksf_1_1comps_1_1ks_device_portal.html#a94f2d902c9447d79a165cdb0ed7417ab',1,'ksf::comps::ksDevicePortal']]],
   ['onwebsockettextmessage_7',['onWebsocketTextMessage',['../classksf_1_1misc_1_1ks_w_s_server.html#a507b6d18fbe0c211bec2bb593173d885',1,'ksf::misc::ksWSServer']]]
