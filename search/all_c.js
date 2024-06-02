73a11bf2358eef9a4d252806fe9c4a4b01ef11b1
cziter15 <cziter15@users.noreply.github.com>
2024-06-02 11:19:03 +0000

    deploy: 8793e10412713c8426f22ff16f231ecd58d0fead

--- search/all_c.js
+++ search/all_c.js
@@ -6,13 +6,14 @@ var searchData=
   ['ondevicemessage_3',['onDeviceMessage',['../classksf_1_1comps_1_1ks_mqtt_connector.html#aff442ccec76a46a0834a21cd882e8ccd',1,'ksf::comps::ksMqttConnector']]],
   ['ondisconnected_4',['onDisconnected',['../classksf_1_1comps_1_1ks_mqtt_connector.html#aacb8a4b214c5ea6d71c74469fec167ce',1,'ksf::comps::ksMqttConnector']]],
   ['onhandleportalcommand_5',['onHandlePortalCommand',['../classksf_1_1comps_1_1ks_device_portal.html#a48dce11cf0fbd3747ab5b5a82db32b54',1,'ksf::comps::ksDevicePortal']]],
-  ['onrequest_5findex_6',['onRequest_index',['../classksf_1_1comps_1_1ks_device_portal.html#a80b467535ec6279ad36ffaa1c181ad90',1,'ksf::comps::ksDevicePortal']]],
-  ['onrequest_5fnotfound_7',['onRequest_notFound',['../classksf_1_1comps_1_1ks_device_portal.html#a8feb8377e4cebd8c15e64071a62ae9fa',1,'ksf::comps::ksDevicePortal']]],
-  ['onrequest_5fotachunk_8',['onRequest_otaChunk',['../classksf_1_1comps_1_1ks_device_portal.html#a874a08f061b795a96dc4badf7415fd82',1,'ksf::comps::ksDevicePortal']]],
-  ['onrequest_5fotafinish_9',['onRequest_otaFinish',['../classksf_1_1comps_1_1ks_device_portal.html#aaf77328746a9a3f01a584ffdf53a90c8',1,'ksf::comps::ksDevicePortal']]],
-  ['onupdateend_10',['onUpdateEnd',['../classksf_1_1comps_1_1ks_device_portal.html#a7dc2e53045a02c604627f8101baa68e2',1,'ksf::comps::ksDevicePortal']]],
-  ['onupdatestart_11',['onUpdateStart',['../classksf_1_1comps_1_1ks_device_portal.html#a94f2d902c9447d79a165cdb0ed7417ab',1,'ksf::comps::ksDevicePortal']]],
-  ['onwebsockettextmessage_12',['onWebsocketTextMessage',['../classksf_1_1misc_1_1ks_w_s_server.html#a507b6d18fbe0c211bec2bb593173d885',1,'ksf::misc::ksWSServer::onWebsocketTextMessage'],['../classksf_1_1comps_1_1ks_device_portal.html#a2992c9c68c1b62d4ddc6e0a64e1edfc7',1,'ksf::comps::ksDevicePortal::onWebsocketTextMessage()']]],
-  ['opening_20existing_20projects_13',['Opening existing projects',['../md_tutorials_2readme.html#autotoc_md21',1,'']]],
-  ['operator_20bool_14',['operator bool',['../classksf_1_1ks_config.html#af05e375f48e5ce57902a587393a95134',1,'ksf::ksConfig']]]
+  ['onreportcustomstats_6',['onReportCustomStats',['../classksf_1_1comps_1_1ks_dev_stat_mqtt_reporter.html#af0e8a72ec0550794dd846ee7e72b5f90',1,'ksf::comps::ksDevStatMqttReporter']]],
+  ['onrequest_5findex_7',['onRequest_index',['../classksf_1_1comps_1_1ks_device_portal.html#a80b467535ec6279ad36ffaa1c181ad90',1,'ksf::comps::ksDevicePortal']]],
+  ['onrequest_5fnotfound_8',['onRequest_notFound',['../classksf_1_1comps_1_1ks_device_portal.html#a8feb8377e4cebd8c15e64071a62ae9fa',1,'ksf::comps::ksDevicePortal']]],
+  ['onrequest_5fotachunk_9',['onRequest_otaChunk',['../classksf_1_1comps_1_1ks_device_portal.html#a874a08f061b795a96dc4badf7415fd82',1,'ksf::comps::ksDevicePortal']]],
+  ['onrequest_5fotafinish_10',['onRequest_otaFinish',['../classksf_1_1comps_1_1ks_device_portal.html#aaf77328746a9a3f01a584ffdf53a90c8',1,'ksf::comps::ksDevicePortal']]],
+  ['onupdateend_11',['onUpdateEnd',['../classksf_1_1comps_1_1ks_device_portal.html#a7dc2e53045a02c604627f8101baa68e2',1,'ksf::comps::ksDevicePortal']]],
+  ['onupdatestart_12',['onUpdateStart',['../classksf_1_1comps_1_1ks_device_portal.html#a94f2d902c9447d79a165cdb0ed7417ab',1,'ksf::comps::ksDevicePortal']]],
+  ['onwebsockettextmessage_13',['onWebsocketTextMessage',['../classksf_1_1misc_1_1ks_w_s_server.html#a507b6d18fbe0c211bec2bb593173d885',1,'ksf::misc::ksWSServer::onWebsocketTextMessage'],['../classksf_1_1comps_1_1ks_device_portal.html#a2992c9c68c1b62d4ddc6e0a64e1edfc7',1,'ksf::comps::ksDevicePortal::onWebsocketTextMessage()']]],
+  ['opening_20existing_20projects_14',['Opening existing projects',['../md_tutorials_2readme.html#autotoc_md21',1,'']]],
+  ['operator_20bool_15',['operator bool',['../classksf_1_1ks_config.html#af05e375f48e5ce57902a587393a95134',1,'ksf::ksConfig']]]
 ];
--- search/variables_9.js
+++ search/variables_9.js
@@ -5,7 +5,8 @@ var searchData=
   ['ondevicemessage_2',['onDeviceMessage',['../classksf_1_1comps_1_1ks_mqtt_connector.html#aff442ccec76a46a0834a21cd882e8ccd',1,'ksf::comps::ksMqttConnector']]],
   ['ondisconnected_3',['onDisconnected',['../classksf_1_1comps_1_1ks_mqtt_connector.html#aacb8a4b214c5ea6d71c74469fec167ce',1,'ksf::comps::ksMqttConnector']]],
   ['onhandleportalcommand_4',['onHandlePortalCommand',['../classksf_1_1comps_1_1ks_device_portal.html#a48dce11cf0fbd3747ab5b5a82db32b54',1,'ksf::comps::ksDevicePortal']]],
-  ['onupdateend_5',['onUpdateEnd',['../classksf_1_1comps_1_1ks_device_portal.html#a7dc2e53045a02c604627f8101baa68e2',1,'ksf::comps::ksDevicePortal']]],
-  ['onupdatestart_6',['onUpdateStart',['../classksf_1_1comps_1_1ks_device_portal.html#a94f2d902c9447d79a165cdb0ed7417ab',1,'ksf::comps::ksDevicePortal']]],
-  ['onwebsockettextmessage_7',['onWebsocketTextMessage',['../classksf_1_1misc_1_1ks_w_s_server.html#a507b6d18fbe0c211bec2bb593173d885',1,'ksf::misc::ksWSServer']]]
+  ['onreportcustomstats_5',['onReportCustomStats',['../classksf_1_1comps_1_1ks_dev_stat_mqtt_reporter.html#af0e8a72ec0550794dd846ee7e72b5f90',1,'ksf::comps::ksDevStatMqttReporter']]],
+  ['onupdateend_6',['onUpdateEnd',['../classksf_1_1comps_1_1ks_device_portal.html#a7dc2e53045a02c604627f8101baa68e2',1,'ksf::comps::ksDevicePortal']]],
+  ['onupdatestart_7',['onUpdateStart',['../classksf_1_1comps_1_1ks_device_portal.html#a94f2d902c9447d79a165cdb0ed7417ab',1,'ksf::comps::ksDevicePortal']]],
+  ['onwebsockettextmessage_8',['onWebsocketTextMessage',['../classksf_1_1misc_1_1ks_w_s_server.html#a507b6d18fbe0c211bec2bb593173d885',1,'ksf::misc::ksWSServer']]]
 ];
