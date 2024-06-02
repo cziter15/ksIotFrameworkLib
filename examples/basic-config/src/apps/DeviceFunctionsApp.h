d78ad00aa5caf680b594add8bb4a8230740c7ab2
Krzysztof Strehlau <chris.streh@gmail.com>
2024-06-02 20:28:15 +0200

    Update DeviceFunctionsApp.h

--- examples/basic-config/src/apps/DeviceFunctionsApp.h
+++ examples/basic-config/src/apps/DeviceFunctionsApp.h
@@ -11,7 +11,7 @@ namespace apps
 			std::weak_ptr<ksf::comps::ksLed> connectionStatusLedWp;							// Weeak pointer to the status LED component.
 			std::weak_ptr<ksf::comps::ksMqttConnector> mqttConnWp;							// Weeak pointer to MQTT component.
 
-			std::unique_ptr<ksf::evt::ksEventHandle> connEventHandleSp, disEventHandleSp;	// Handles to MQTT events.
+			std::unique_ptr<ksf::evt::ksEventHandle> connEventHandle, disEventHandle;				// Handles to MQTT events.
 
 			void onMqttConnected();
 			void onMqttDisconnected();
