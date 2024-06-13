31fd57e3ddf7e52b4927a66603db7536f762e341
cziter15 <cziter15@users.noreply.github.com>
2024-06-13 06:39:01 +0000

    deploy: c8f44cadfe1335ceaf66e312f1f3c25aa2613174

--- search/all_10.js
+++ search/all_10.js
@@ -3,5 +3,6 @@ var searchData=
   ['the_20application_0',['🌱 Building the application',['../index.html#autotoc_md8',1,'']]],
   ['the_20hood_1',['🔎 How does it work under the hood?',['../index.html#autotoc_md9',1,'']]],
   ['triggered_2',['triggered',['../classksf_1_1ks_simple_timer.html#a5d12020dc37e1836c9c98ec92eade407',1,'ksf::ksSimpleTimer']]],
-  ['triggerstate_3',['triggerState',['../classksf_1_1comps_1_1ks_reset_button.html#a19333b9f885a6e61c27e04f206b75cb7',1,'ksf::comps::ksResetButton']]]
+  ['triggerfactoryreset_3',['triggerFactoryReset',['../classksf_1_1comps_1_1ks_device_portal.html#a035eda5d01f853d77445fd4b75d74909',1,'ksf::comps::ksDevicePortal']]],
+  ['triggerstate_4',['triggerState',['../classksf_1_1comps_1_1ks_reset_button.html#a19333b9f885a6e61c27e04f206b75cb7',1,'ksf::comps::ksResetButton']]]
 ];
