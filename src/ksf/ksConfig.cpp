6628ee4b5cbd69d1a855849e7fd81c1573803524
cziter15 <chris.streh@gmail.com>
2025-02-03 21:18:52 +0100

    Fix esp8266 compile error

--- src/ksf/ksConfig.cpp
+++ src/ksf/ksConfig.cpp
@@ -31,7 +31,7 @@ namespace ksf
 		configPath += fileName;
 
 		/* Read config file. */
-		if (auto fileReader{LittleFS.open(configPath.c_str())}; fileReader)
+		if (auto fileReader{LittleFS.open(configPath.c_str(), PSTR("r"))})
 		{
 			/* Read until EOF. */
 			while (fileReader.available())
@@ -75,19 +75,16 @@ namespace ksf
 		if (!isDirty)
 			return;
 
-		/* Open file for writing */
-		File fileWriter = LittleFS.open(configPath.c_str(), "w", true);
-		if (!fileWriter) 
+		/* If marked dirty, save changes to FS. */
+		auto fileWriter{LittleFS.open(configPath.c_str(), PSTR("w"))};
+		if (!fileWriter)
 			return;
 
-		/* Write parameters */
-		for (const auto& [name, value] : configParams) 
+		for (const auto&[name, value] : configParams)
 		{
 			fileWriter.println(name.c_str());
 			fileWriter.println(value.c_str());
 		}
-
-		fileWriter.flush();
 		fileWriter.close();
 	}
 }
