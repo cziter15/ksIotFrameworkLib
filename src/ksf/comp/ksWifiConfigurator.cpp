5562fbc51ed626123343f9c4cfb13e576ba51aab
cziter15 <chris.streh@gmail.com>
2025-02-15 00:23:43 +0100

    Fix error compile

--- src/ksf/comp/ksWifiConfigurator.cpp
+++ src/ksf/comp/ksWifiConfigurator.cpp
@@ -27,7 +27,8 @@
 
 namespace ksf::comps
 {
-	ksWifiConfigurator::ksWifiConfigurator() : ksWifiConfigurator(PSTR("KSFDevice"))
+	ksWifiConfigurator::ksWifiConfigurator() 
+		: ksWifiConfigurator(PSTR("KSFDevice"))
 	{}
 
 	ksWifiConfigurator::ksWifiConfigurator(std::string devicePrefixName)
