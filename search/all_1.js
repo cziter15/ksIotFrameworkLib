c01ef91d0fc8cc0be853ff4f79b49989481b3e0e
cziter15 <cziter15@users.noreply.github.com>
2025-02-14 23:01:04 +0000

    deploy: c34e6102816c675f9fa7ae40963dcb050bb580d2

--- search/all_1.js
+++ search/all_1.js
@@ -4,6 +4,6 @@ var searchData=
   ['blinkintervalms_1',['blinkIntervalMs',['../classksf_1_1comps_1_1ks_led.html#aad7cd66a8d631b1f1d94b61d76bbf372',1,'ksf::comps::ksLed']]],
   ['blinkloops_2',['blinkLoops',['../classksf_1_1comps_1_1ks_led.html#a0a222a2588b3bc63ff1df4d671f99653',1,'ksf::comps::ksLed']]],
   ['breakapp_3',['breakApp',['../classksf_1_1comps_1_1ks_device_portal.html#a573eb03602ce8a4352af1963ff80b748',1,'ksf::comps::ksDevicePortal']]],
-  ['broadcast_4',['broadcast',['../classksf_1_1evt_1_1ks_event.html#ac672facd2bd8e5084f8251b228d2cf4f',1,'ksf::evt::ksEvent']]],
+  ['broadcast_4',['broadcast',['../classksf_1_1evt_1_1ks_event.html#af5b0ecfe3c6f6c4093e316c8d3c7299f',1,'ksf::evt::ksEvent']]],
   ['building_20the_20application_5',['🌱 Building the application',['../index.html#autotoc_md8',1,'']]]
 ];
--- search/functions_1.js
+++ search/functions_1.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['begin_0',['begin',['../classksf_1_1misc_1_1ks_w_s_server.html#a30264ac138648d3ee913519cec7c5920',1,'ksf::misc::ksWSServer']]],
-  ['broadcast_1',['broadcast',['../classksf_1_1evt_1_1ks_event.html#ac672facd2bd8e5084f8251b228d2cf4f',1,'ksf::evt::ksEvent']]]
+  ['broadcast_1',['broadcast',['../classksf_1_1evt_1_1ks_event.html#af5b0ecfe3c6f6c4093e316c8d3c7299f',1,'ksf::evt::ksEvent']]]
 ];
