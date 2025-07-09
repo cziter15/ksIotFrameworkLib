6bd3b0735e2c93ba5c126728e9cf33d5a2f182f9
cziter15 <chris.streh@gmail.com>
2025-07-09 21:44:59 +0200

    Fix copyright

--- platformio/logger.py
+++ platformio/logger.py
@@ -1,5 +1,5 @@
 # flake8: noqa
-# Copyright (c) 2021-2025, Krzysztof Strehlau
+# Copyright (c) 2020-2025, Krzysztof Strehlau
 # This file is part of the ksIotFramework library.
 # All licensing information can be found inside LICENSE.md file
 # https://github.com/cziter15/ksIotFrameworkLib/blob/master/LICENSE
--- src/ksf/evt/ksEventHandle.h
+++ src/ksf/evt/ksEventHandle.h
@@ -1,5 +1,5 @@
 /*
- *	Copyright (c) 2021-2025, Krzysztof Strehlau
+ *	Copyright (c) 2020-2025, Krzysztof Strehlau
  *
  *	This file is a part of the ksIotFramework library.
  *	All licensing information can be found inside LICENSE.md file.
