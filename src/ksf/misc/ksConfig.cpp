aa675b037b6f744ac964cc98efb4145444d32e09
cziter15 <chris.streh@gmail.com>
2025-09-22 21:38:12 +0200

    Fix comp err

--- src/ksf/misc/ksConfig.cpp
+++ src/ksf/misc/ksConfig.cpp
@@ -20,63 +20,64 @@
 	ksConfig::ksConfig(const std::string& fileName)
 		: isDirty{false}, configPath{getNvsDirectory()}
 	{
-	/* If no file specified, clear internal path. */
-	if (fileName.empty())
-	{
-		configPath.clear();
-		return;
-	}
-
-	/* Add leading slash and assemble file path. */
-	if (fileName[0] != '/')
-		configPath += '/';
-	configPath += fileName;
-
-	/* Read config file. */
-	File fileReader{LittleFS.open(configPath.c_str(), "r")};
-	if (fileReader)
-	{
-		/* Use std::array for fixed buffer with automatic memory management */
-		constexpr size_t bufferSize{256};
-		std::array<char, bufferSize> buffer{};
-		
-		std::string key;
-		bool isReadingKey{true};
-		
-		while (fileReader.available())
+		/* If no file specified, clear internal path. */
+		if (fileName.empty())
 		{
-			/* Ensure we leave space for null terminator */
-			size_t maxReadSize{buffer.size() - 1};
-			size_t bytesRead{fileReader.readBytesUntil('\n', buffer.data(), maxReadSize)};
+			configPath.clear();
+			return;
+		}
+
+		/* Add leading slash and assemble file path. */
+		if (fileName[0] != '/')
+			configPath += '/';
+		configPath += fileName;
+
+		/* Read config file. */
+		File fileReader{LittleFS.open(configPath.c_str(), "r")};
+		if (fileReader)
+		{
+			/* Use std::array for fixed buffer with automatic memory management */
+			constexpr size_t bufferSize{256};
+			std::array<char, bufferSize> buffer{};
 			
-			/* Safety check to prevent buffer overrun */
-			if (bytesRead >= maxReadSize)
-				bytesRead = maxReadSize;
+			std::string key;
+			bool isReadingKey{true};
 			
-			/* Ensure null termination */
-			buffer[bytesRead] = '\0';
-			
-			/* Remove trailing CR if present */
-			if (bytesRead > 0 && buffer[bytesRead - 1] == '\r')
-				buffer[bytesRead - 1] = '\0';
-			
-			/* Use string_view to avoid unnecessary copying */
-			std::string_view line{buffer.data()};
-			
-			if (isReadingKey)
+			while (fileReader.available())
 			{
-				key = line;
-				isReadingKey = false;
-			}
-			else
-			{
-				configParams[key] = std::string{line};
-				isReadingKey = true;
+				/* Ensure we leave space for null terminator */
+				size_t maxReadSize{buffer.size() - 1};
+				size_t bytesRead{fileReader.readBytesUntil('\n', buffer.data(), maxReadSize)};
+				
+				/* Safety check to prevent buffer overrun */
+				if (bytesRead >= maxReadSize)
+					bytesRead = maxReadSize;
+				
+				/* Ensure null termination */
+				buffer[bytesRead] = '\0';
+				
+				/* Remove trailing CR if present */
+				if (bytesRead > 0 && buffer[bytesRead - 1] == '\r')
+					buffer[bytesRead - 1] = '\0';
+				
+				/* Use string_view to avoid unnecessary copying */
+				std::string_view line{buffer.data()};
+				
+				if (isReadingKey)
+				{
+					key = line;
+					isReadingKey = false;
+				}
+				else
+				{
+					configParams[key] = std::string{line};
+					isReadingKey = true;
+				}
 			}
 		}
 		fileReader.close();
 	}
-
+	
 	ksConfig::~ksConfig()
 	{
 		if (!isDirty || configPath.empty())
