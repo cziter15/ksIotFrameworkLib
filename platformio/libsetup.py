8b2d033a226ee2c86edd5850efcb333a00bddd9e
Krzysztof Strehlau <chris.streh@gmail.com>
2025-10-04 22:13:05 +0200

    Refactor logging function names in libsetup.py

--- platformio/libsetup.py
+++ platformio/libsetup.py
@@ -1,25 +1,24 @@
 # flake8: noqa
-# Copyright (c) 2020-2026, Krzysztof Strehlau
+# Copyright (c) 2020-2025, Krzysztof Strehlau
 # This file is part of the ksIotFrameworkLib IoT library.
 # All licensing information can be found inside LICENSE.md file
 # https://github.com/cziter15/ksIotFrameworkLib/blob/master/LICENSE
-from logger import ksPrintLog, Colors
-ksPrintLog(Colors.Green, "Running extra script for library.")
+from logger import ks_print_log, Colors
+ks_print_log(Colors.Green, "Running extra script for library.")
 try:
+	from SCons.Script import Import, DefaultEnvironment
 	Import("projenv", "env")
-	ksPrintLog(Colors.Magenta, "Building library environment list.")
+	ks_print_log(Colors.Magenta, "Building library environment list.")
 	environments = [env, DefaultEnvironment(), projenv]
 	for lb in env.GetLibBuilders():
 		environments.append(lb.env)
 	for e in environments:
-		# remove unused flags
 		e.ProcessUnFlags("-std=gnu++11")
-		# add flags to environment
 		e.ProcessFlags("-std=gnu++2a")
 		e.ProcessFlags("-DNO_GLOBAL_ARDUINO_OTA=1")
 		e.ProcessFlags("-DWEBSOCKETS_SAVE_RAM=1")
-	ksPrintLog(Colors.Magenta, "Successfully tweaked platform settings for [" + str(len(environments)) + "] environments.")
-	ksPrintLog(Colors.Green, "Extra script finished.")
+	ks_print_log(Colors.Magenta, "Successfully tweaked platform settings for [" + str(len(environments)) + "] environments.")
+	ks_print_log(Colors.Green, "Extra script finished.")
 except BaseException as err:
-	ksPrintLog(Colors.Red, "Error while executing script. Are you on newest platformio core?")
-	ksPrintLog(Colors.Yellow, str(err))
+	ks_print_log(Colors.Red, "Error while executing script. Are you on newest platformio core?")
+	ks_print_log(Colors.Yellow, str(err))
