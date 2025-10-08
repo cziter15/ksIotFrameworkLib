da7d2cf354fa70739e67fbd137756040249d99f0
cziter15 <cziter15@users.noreply.github.com>
2025-10-08 10:35:26 +0000

    deploy: 2ae591b7ef5a92fffc7f3a71c5ef89300eb92a0f

--- search/all_4.js
+++ search/all_4.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['eventbasewp_0',['eventBaseWp',['../classksf_1_1evt_1_1ks_event_handle.html#a21faf24eac17f0bdbe702459ef8d7c31',1,'ksf::evt::ksEventHandle']]],
-  ['examples_1',['📟 Examples',['../index.html#autotoc_md10',1,'']]]
+  ['emptystring_0',['emptyString',['../classksf_1_1misc_1_1ks_config.html#ac62b2c70784f9dfea5b0b5df7cd25ec9',1,'ksf::misc::ksConfig']]],
+  ['eventbasewp_1',['eventBaseWp',['../classksf_1_1evt_1_1ks_event_handle.html#a21faf24eac17f0bdbe702459ef8d7c31',1,'ksf::evt::ksEventHandle']]],
+  ['examples_2',['📟 Examples',['../index.html#autotoc_md10',1,'']]]
 ];
--- search/variables_4.js
+++ search/variables_4.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['eventbasewp_0',['eventBaseWp',['../classksf_1_1evt_1_1ks_event_handle.html#a21faf24eac17f0bdbe702459ef8d7c31',1,'ksf::evt::ksEventHandle']]]
+  ['emptystring_0',['emptyString',['../classksf_1_1misc_1_1ks_config.html#ac62b2c70784f9dfea5b0b5df7cd25ec9',1,'ksf::misc::ksConfig']]],
+  ['eventbasewp_1',['eventBaseWp',['../classksf_1_1evt_1_1ks_event_handle.html#a21faf24eac17f0bdbe702459ef8d7c31',1,'ksf::evt::ksEventHandle']]]
 ];
